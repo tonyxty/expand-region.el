d1ae49542bc1ede099f2c35b347b3ca38ca42b78
Tony Beta Lambda <tonybetalambda@gmail.com>
2021-01-26 01:48:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# expand-region.el
+
+Expands current region in emacs, much like the "Extend Selection", or Ctrl-W, command found in Intellij-based IDEs.
+
+I recommend binding `expand-region` to `C-w` to get Intellij-like behavior, and bind `kill-region` to `C-d`.
+
+[Screencast](https://asciinema.org/a/386993)
---
+++ expand-region.el
@@ -0,0 +1,36 @@
+(defvar expand-region-all-things '(word symbol uuid filename url sexp list defun page))
+(defvar expand-region-start-point)
+(defvar expand-region-things)
+(defun expand-region ()
+  (interactive)
+  (when (not (eq last-command this-command))
+    (setq expand-region-start-point (point))
+    (setq expand-region-things expand-region-all-things))
+  (let ((region-active (use-region-p))
+	(region-start (region-beginning))
+	(region-end (region-end))
+	thing)
+    (goto-char expand-region-start-point)
+    (catch 'loop-things
+      (while expand-region-things
+	(setq thing (car expand-region-things)
+	      expand-region-things (cdr expand-region-things))
+	(let* ((bounds (bounds-of-thing-at-point thing))
+	       (start (car bounds))
+	       (end (cdr bounds)))
+  	  (when (and start end ; the thing must be non-nil
+	       ; and it must properly extend the current region, i.e.,
+		     (or (not region-active) ; either no region is active
+			 (and (<= start region-start) ; or it properly contains the region
+			      (>= end region-end)
+			      (or (< start region-start) (> end region-end)))))
+	    (push-mark start t t)
+	    (goto-char end)
+	    (throw 'loop-things thing)))))
+    ; Return to starting point when list exhausted
+    (when (not thing)
+      (deactivate-mark)
+      (setq expand-region-things expand-region-all-things))
+    (message "%s" thing)))
+
+(provide 'expand-region)
